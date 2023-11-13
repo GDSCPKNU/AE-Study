e0bed40b9c4b8a96fc012e5f32e4052e0493c675
Sion <zktl3575@gmail.com>
2023-11-13 16:23:12 +0900

    [3주차] 김수현 - 오큰수 풀이

---
+++ ksh/week3/오큰수.java
@@ -0,0 +1,35 @@
+package week3;
+
+import java.io.*;
+import java.util.*;
+
+class 오큰수 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Stack<Integer> stack = new Stack<>();
+
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < n; i++) {
+            while (!stack.empty() && arr[stack.peek()] < arr[i]) {
+                arr[stack.pop()] = arr[i];
+            }
+            stack.push(i);
+        }
+
+        while (!stack.empty()) {
+            arr[stack.pop()] = -1;
+        }
+
+        for (int num : arr) {
+            bw.write(num + " ");
+        }
+        bw.close();
+    }
+}
