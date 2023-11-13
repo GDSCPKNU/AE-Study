efa40be7826afa9b68afecfe7aea08738404f5b0
Sion <zktl3575@gmail.com>
2023-11-13 14:27:52 +0900

    [3주차 - 2차] 김수현 - 스택 수열 풀이

--- ksh/week3/스택_수열.java
+++ ksh/week3/스택_수열.java
@@ -2,56 +2,33 @@ package week3;
 
 import java.io.*;
 import java.util.Stack;
-import java.util.List;
-import java.util.ArrayList;
 
 public class 스택_수열 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         Stack<Integer> stack = new Stack<>();
+
         int n = Integer.parseInt(br.readLine());
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
 
-        List<String> answer = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
 
         int count = 1;
         for (int num : arr) {
-            if (stack.empty()) {
-                for (int i = 0; i < num; i++) {
-                    stack.push(count++);
-                    answer.add("+");
-                }
-            }
-            while (stack.peek() < num) {
+            while (stack.empty() || stack.peek() < num) {
                 stack.push(count++);
-                answer.add("+");
+                sb.append("+\n");
             }
             if (stack.peek() != num) {
-                bw.write("NO");
-                bw.close();
+                System.out.println("NO");
                 return;
             }
             stack.pop();
-            answer.add("-");
+            sb.append("-\n");
         }
-
-        for (String ans : answer) {
-            bw.write(ans);
-            bw.newLine();
-        }
-
-        bw.close();
+        System.out.println(sb);
     }
 }
-
-
-
-
-
-
-
-
