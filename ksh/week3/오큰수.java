9f176424c5453f668509db197add9b50a3629528
Sion <zktl3575@gmail.com>
2023-11-17 13:34:17 +0900

    [3주차] 김수현 - 오큰수 풀이

--- ksh/week3/오큰수.java
+++ ksh/week3/오큰수.java
@@ -1,35 +1,37 @@
 package week3;
 
 import java.io.*;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class 오큰수 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Stack<Integer> stack = new Stack<>();
 
         int n = Integer.parseInt(br.readLine());
-
         StringTokenizer st = new StringTokenizer(br.readLine());
         int[] arr = new int[n];
-        boolean[] hasBiggerThanMe = new boolean[n];
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
         for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (arr[i] < arr[j]) {
-                    bw.write(arr[j] + " ");
-                    hasBiggerThanMe[i] = true;
-                    break;
-                }
+            while (!stack.empty() && arr[stack.peek()] < arr[i]) {
+                arr[stack.pop()] = arr[i];
             }
-            if (!hasBiggerThanMe[i]) {
-                bw.write(-1 + " ");
-            }
+            stack.push(i);
         }
 
+        while (!stack.empty()) {
+            arr[stack.pop()] = -1;
+        }
+
+
+        for (int num : arr) {
+            bw.write(num + " ");
+        }
         bw.close();
     }
 }
